07e1adbc36b63c12218e4db20e995bfcc7780855
Daniil Kulak <kulakdaniil@gmail.com>
2019-08-18 02:39:50 +0700

    HA - Знакомство с Kotlin. Реализация utils методов и extensions в Kotlin (stripHtml)

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,9 +1,15 @@
 package ru.skillbranch.devintensive.extensions
 
+
+
 fun String.truncate(count: Int = 16): String {
     val input = trim()
     return when {
         input.length < count -> input
-        else -> "${if (input[count - 1] == ' ') input.substring(0, count - 1) else input.substring(0, count)}..."
+        else -> "${input.substring(0, if (input[count - 1] == ' ') count - 1 else count)}..."
     }
 }
+
+// заменяем все множественные пробелы на одинарный и удаляем все html-теги + html-escape последовательности
+fun String.stripHtml(): String = replace( "<.*?>|&amp;|&gt;|&lt;|&quot;|&apos;|&nbsp;".toRegex(), "")
+    .replace("\\s+".toRegex(), " ")
