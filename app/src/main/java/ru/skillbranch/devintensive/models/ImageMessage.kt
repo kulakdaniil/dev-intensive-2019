a2576f1d477158cb2ff57b1706459b3e2025ad7a
Daniil Kulak <kulakdaniil@gmail.com>
2019-08-18 10:35:34 +0700

    HA - Знакомство с Kotlin. Реализация utils методов и extensions в Kotlin (Base Message, AbstractFactory)

--- app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.models
 
+import ru.skillbranch.devintensive.extensions.humanizeDiff
 import java.util.*
 
 class ImageMessage(
@@ -10,6 +11,6 @@ class ImageMessage(
     date: Date = Date(),
     var image: String?
 ) : BaseMessage(id, from, chat, isIncoming, date) {
-    override fun formatMessage(): String = "id:$id ${from?.firstName} " +
-            "${if (isIncoming)  "получил" else "отправил"} изображение \"$image\""
+    override fun formatMessage(): String = "${from?.firstName} " +
+            "${if (isIncoming)  "получил" else "отправил"} изображение \"$image\" ${date.humanizeDiff()}"
 }
